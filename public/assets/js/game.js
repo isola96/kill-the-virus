78425a198e726025f00ec83c7498c7269b578b53
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-03-17 17:17:04 +0100

    added online-users and and removed old code

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,41 +1,31 @@
 const socket = io();
 
 const startPageEl = document.querySelector('#startPage');
+const gameEl = document.querySelector('#game');
+const waitingForOpponentEl = document.querySelector('#waitingForOpponent');
+const playersOnline = document.querySelector('#online-users');
 const usernameForm = document.querySelector('#usernameForm');
 const usernameInput = document.querySelector('#username');
-const waitingForOpponentEl = document.querySelector('#waitingForOpponent');
-const gameEl = document.querySelector('#game');
-
 
 let users = [];
-// let users = [{username: Elin, room: 1}, {username: Johanna, room: 1}, {username: Jooheon, room: 2}, {username: IM, room: 2}];
-// let room = null;
-// let rounds = 0;
+
+// tell server that another user has connected
+socket.on('user:connected', () => {
+    console.log('someone connected') 
+});
 
 usernameForm.addEventListener('submit', e => {
     e.preventDefault();
 
     const username = usernameInput.value;
 
-    // if username-input is empty; return
-    if (!username) {
-		return;
-	}
     console.log('someone put username as: ', username);
 
     users.push(username);   // skicka detta till servern? för att sparas hos andra också?
     // socket.emit('user:waiting', username, )
 
-    // see if there's a player waiting 
-    // create a new room for the player to wait in for an opponent
-
-
-    // tell server a user is ready to... wait
-    // socket.on('user:joined',)
-
-    // tell server that another user has connected
-    socket.on('user:connected', () => {
-        console.log('someone connected') 
+    socket.emit('user:waiting', username, (status) => {
+		console.log("Waiting for a user", status);
     });
 
     // toggle classlist
@@ -44,11 +34,10 @@ usernameForm.addEventListener('submit', e => {
 
     // någon kod för att vänta på en till spelare
     
-
     // skicka prompt och fråga om spelare är redo
     alert('Click ok to start game!');
 
+    // show game and hide waiting
     gameEl.classList.toggle('hide');
-
-
+    waitingForOpponentEl.classList.toggle('hide');
 });
